6c810aa45b9607501e28901b53febc5a6731931a
Arpit Sachan <arpitsachan46@gmail.com>
2020-07-21 17:07:00 +0530

    api key removed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 
 
 class Constants {
-  static String TOP_HEADLINES_URL= "https://newsapi.org/v2/top-headlines?country=us&apiKey=8645e2654f8047f392baf4295138d032";
+  static String TOP_HEADLINES_URL= "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY";
 
 
   static String headlinesfor(String keyword){
-    return "https://newsapi.org/v2/everything?q=$keyword&apiKey=8645e2654f8047f392baf4295138d032";
+    return "https://newsapi.org/v2/everything?q=$keyword&apiKey=YOUR_API_KEY";
   }
 }
